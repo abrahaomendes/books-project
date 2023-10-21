edb133c5a85e81e994146896aad0ce133561ced2
Abraão Mendes <abraaomneto@gmail.com>
2023-10-21 20:54:51 -0300

    Implementação da camada de domínio

---
+++ src/main/java/me/dio/booksproject/domain/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package me.dio.booksproject.domain.repository;
+
+import me.dio.booksproject.domain.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
